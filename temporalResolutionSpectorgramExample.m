1b84b19eee424c4ad84b2c10e0bfb0cf9a93ad4a
fltodd <fraser.todd.501@gmail.com>
2023-04-25 16:23:25 +0100

    Got a good set of figures for temporal Resolution

--- temporalResolutionSpectorgramExample.m
+++ temporalResolutionSpectorgramExample.m
@@ -2,17 +2,18 @@ clearvars;
 
 fs = 44100;
 freq = 1000;
-lengthSeconds = 2;
+lengthSeconds = 0.2;
+windowSize = 1024;
 
 baseSine = sin(linspace(0, 2*pi*freq*lengthSeconds, fs*lengthSeconds));
 harmSine = sin(linspace(0, 2*pi*2*freq*lengthSeconds*0.5, fs*lengthSeconds*0.5));
 padding = zeros(1, (length(baseSine)-length(harmSine))/2 + 1);
 signal = baseSine + cat(2, padding, harmSine, padding(1:length(padding)-2));
 
-window = hannWindow(1024, 1024);
+window = hannWindow(windowSize, windowSize);
 
 figure
-spectrogramPSD(signal, fs, window, freq*3, -60, 0.5, yScale='lin');
+spectrogramPSD(signal, fs, window, freq*3, -60, 0, yScale='lin');
 
 figure
 spectrogramPSD(signal, fs, window, freq*3, -60, 0.5, yScale='lin');
